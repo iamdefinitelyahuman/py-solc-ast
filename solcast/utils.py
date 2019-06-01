2c72e378d7b9dc325051b014b8b7ffd16da43a38
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-06-01 02:07:09 +0100

    handle recursive library dependencies, comments

--- solcast/utils.py
+++ solcast/utils.py
@@ -21,18 +21,34 @@ def set_dependencies(source_nodes):
     Returns: SourceUnit objects where all ContractDefinition nodes contain
              'dependencies' and 'libraries' attributes.'''
     symbol_map = get_symbol_map(source_nodes)
+
+    # add immediate dependencies
     for contract in [x for i in source_nodes for x in i]:
         contract.dependencies = set()
+
+        # listed dependencies
         for key in contract._node['contractDependencies']:
             contract.dependencies.add(symbol_map[key])
+
+        # using .. for libraries
         for node in [i for i in contract._node['nodes'] if i['nodeType'] == "UsingForDirective"]:
             contract._node['nodes'].remove(node)
             id_ = node['libraryName']['referencedDeclaration']
             contract.libraries[node['typeName']['name']] = symbol_map[id_]
             contract.dependencies.add(symbol_map[id_])
+
+        # unlinked libraries
         for node in contract.children(node_type="Identifier"):
             if node.reference in symbol_map and symbol_map[node.reference].type == "library":
                 contract.dependencies.add(symbol_map[node.reference])
+
+    # add recursive dependencies
+    for contract in [x for i in source_nodes for x in i]:
+        for node in contract.dependencies.copy():
+            contract.dependencies |= node.dependencies
+
+    # convert dependency sets to lists
+    for contract in [x for i in source_nodes for x in i]:
         contract.dependencies = sorted(contract.dependencies, key=lambda k: k.name)
     return source_nodes
 
