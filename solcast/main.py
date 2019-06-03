59f914270e1c79bd16073247114ff91039593175
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-06-03 09:30:09 +0200

    move set_dependencies to main

--- solcast/main.py
+++ solcast/main.py
@@ -6,9 +6,24 @@ from pathlib import Path
 from .bases import NodeBase, ListNodeBase
 from .definitions import ContractDefinition
 from .utils import (
-    set_dependencies,
     get_node_objects,
+    get_symbol_map
 )
+
+
+class SourceUnit(NodeBase, ListNodeBase):
+
+    def __init__(self, node):
+        if 'ast' in node:
+            node = node['ast']
+        self.depth = 0
+        super().__init__(node, None)
+        if node['nodeType'] != "SourceUnit":
+            raise ValueError("Wrong node type - {}".format(node['nodeType']))
+        self.name = node['absolutePath']
+        self.path = node['absolutePath']
+        self.contracts = get_node_objects(node, "ContractDefinition", ContractDefinition, self)
+        ListNodeBase.__init__(self, self.contracts)
 
 
 def from_standard_output_json(path):
@@ -30,16 +45,42 @@ def from_standard_output(output_json):
     return source_nodes
 
 
-class SourceUnit(NodeBase, ListNodeBase):
+def set_dependencies(source_nodes):
+    '''Sets contract node dependencies.
 
-    def __init__(self, node):
-        if 'ast' in node:
-            node = node['ast']
-        self.depth = 0
-        super().__init__(node, None)
-        if node['nodeType'] != "SourceUnit":
-            raise ValueError("Wrong node type - {}".format(node['nodeType']))
-        self.name = node['absolutePath']
-        self.path = node['absolutePath']
-        self.contracts = get_node_objects(node, "ContractDefinition", ContractDefinition, self)
-        ListNodeBase.__init__(self, self.contracts)
+    Arguments:
+        source_nodes: list of SourceUnit objects.
+
+    Returns: SourceUnit objects where all ContractDefinition nodes contain
+             'dependencies' and 'libraries' attributes.'''
+    symbol_map = get_symbol_map(source_nodes)
+
+    # add immediate dependencies
+    for contract in [x for i in source_nodes for x in i]:
+        contract.dependencies = set()
+
+        # listed dependencies
+        for key in contract._node['contractDependencies']:
+            contract.dependencies.add(symbol_map[key])
+
+        # using .. for libraries
+        for node in [i for i in contract._node['nodes'] if i['nodeType'] == "UsingForDirective"]:
+            contract._node['nodes'].remove(node)
+            id_ = node['libraryName']['referencedDeclaration']
+            contract.libraries[node['typeName']['name']] = symbol_map[id_]
+            contract.dependencies.add(symbol_map[id_])
+
+        # unlinked libraries
+        for node in contract.children(filters={'node_type': "Identifier"}):
+            if node.reference in symbol_map and symbol_map[node.reference].type == "library":
+                contract.dependencies.add(symbol_map[node.reference])
+
+    # add recursive dependencies
+    for contract in [x for i in source_nodes for x in i]:
+        for node in contract.dependencies.copy():
+            contract.dependencies |= node.dependencies
+
+    # convert dependency sets to lists
+    for contract in [x for i in source_nodes for x in i]:
+        contract.dependencies = sorted(contract.dependencies, key=lambda k: k.name)
+    return source_nodes
--- solcast/utils.py
+++ solcast/utils.py
@@ -12,47 +12,6 @@ def get_symbol_map(source_nodes):
     return symbol_map
 
 
-def set_dependencies(source_nodes):
-    '''Sets contract node dependencies.
-
-    Arguments:
-        source_nodes: list of SourceUnit objects.
-
-    Returns: SourceUnit objects where all ContractDefinition nodes contain
-             'dependencies' and 'libraries' attributes.'''
-    symbol_map = get_symbol_map(source_nodes)
-
-    # add immediate dependencies
-    for contract in [x for i in source_nodes for x in i]:
-        contract.dependencies = set()
-
-        # listed dependencies
-        for key in contract._node['contractDependencies']:
-            contract.dependencies.add(symbol_map[key])
-
-        # using .. for libraries
-        for node in [i for i in contract._node['nodes'] if i['nodeType'] == "UsingForDirective"]:
-            contract._node['nodes'].remove(node)
-            id_ = node['libraryName']['referencedDeclaration']
-            contract.libraries[node['typeName']['name']] = symbol_map[id_]
-            contract.dependencies.add(symbol_map[id_])
-
-        # unlinked libraries
-        for node in contract.children(filters={'node_type': "Identifier"}):
-            if node.reference in symbol_map and symbol_map[node.reference].type == "library":
-                contract.dependencies.add(symbol_map[node.reference])
-
-    # add recursive dependencies
-    for contract in [x for i in source_nodes for x in i]:
-        for node in contract.dependencies.copy():
-            contract.dependencies |= node.dependencies
-
-    # convert dependency sets to lists
-    for contract in [x for i in source_nodes for x in i]:
-        contract.dependencies = sorted(contract.dependencies, key=lambda k: k.name)
-    return source_nodes
-
-
 def get_node_objects(node, node_type, class_, parent):
     '''Creates node objects from a list of nodes.
 
