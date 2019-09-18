92602f653fe1d1c17c4fcfa736265ffba41c336d
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-09-18 22:24:40 +0800

    linting

--- solcast/dependencies.py
+++ solcast/dependencies.py
@@ -2,36 +2,37 @@
 
 
 def set_dependencies(source_nodes):
-    '''Sets contract node dependencies.
+    """Sets contract node dependencies.
 
     Arguments:
         source_nodes: list of SourceUnit objects.
 
     Returns: SourceUnit objects where all ContractDefinition nodes contain
-             'dependencies' and 'libraries' attributes.'''
+             'dependencies' and 'libraries' attributes."""
     symbol_map = get_symbol_map(source_nodes)
     contract_list = [x for i in source_nodes for x in i if x.nodeType == "ContractDefinition"]
 
     # add immediate dependencies
     for contract in contract_list:
         contract.dependencies = set()
-        contract.libraries = dict((
-            _get_type_name(i.typeName),
-            i.libraryName.name
-        ) for i in contract.nodes if i.nodeType == "UsingForDirective")
+        contract.libraries = dict(
+            (_get_type_name(i.typeName), i.libraryName.name)
+            for i in contract.nodes
+            if i.nodeType == "UsingForDirective"
+        )
 
         # listed dependencies
         for key in contract.contractDependencies:
             contract.dependencies.add(symbol_map[key])
 
         # using .. for libraries
-        for node in contract.children(filters={'nodeType': "UsingForDirective"}):
+        for node in contract.children(filters={"nodeType": "UsingForDirective"}):
             id_ = node.libraryName.referencedDeclaration
             contract.libraries[_get_type_name(node.typeName)] = symbol_map[id_]
             contract.dependencies.add(symbol_map[id_])
 
         # unlinked libraries
-        for node in contract.children(filters={'node_type': "Identifier"}):
+        for node in contract.children(filters={"node_type": "Identifier"}):
             if node.reference in symbol_map and symbol_map[node.reference].type == "library":
                 contract.dependencies.add(symbol_map[node.reference])
 
@@ -47,10 +48,10 @@ def set_dependencies(source_nodes):
 
 
 def get_symbol_map(source_nodes):
-    '''Generates a dict of {'id': SourceUnit} used for linking nodes.
+    """Generates a dict of {'id': SourceUnit} used for linking nodes.
 
     Arguments:
-        source_nodes: list of SourceUnit objects.'''
+        source_nodes: list of SourceUnit objects."""
     symbol_map = {}
     for node in source_nodes:
         symbol_map.update((v[0], node[k]) for k, v in node.exportedSymbols.items())
@@ -60,8 +61,8 @@ def get_symbol_map(source_nodes):
 def _get_type_name(node):
     if node is None:
         return None
-    if hasattr(node, 'name'):
+    if hasattr(node, "name"):
         return node.name
-    if hasattr(node, 'typeDescriptions'):
-        return node.typeDescriptions.typeString
+    if hasattr(node, "typeDescriptions"):
+        return node.typeDescriptions["typeString"]
     return None
--- solcast/nodes.py
+++ solcast/nodes.py
@@ -4,16 +4,15 @@ import functools
 
 
 class NodeBase:
-
     def __init__(self, ast, parent):
         self.depth = parent.depth + 1 if parent is not None else 0
         self._parent = parent
         self._children = set()
-        src = ast['src']
-        self.offset = (src[0], src[0]+src[1])
+        src = ast["src"]
+        self.offset = (src[0], src[0] + src[1])
         for key, value in ast.items():
-            if isinstance(value, dict) and value.get('nodeType') == "Block":
-                value = value['statements']
+            if isinstance(value, dict) and value.get("nodeType") == "Block":
+                value = value["statements"]
             elif key == "body" and not value:
                 value = []
             if isinstance(value, dict):
@@ -24,7 +23,7 @@ class NodeBase:
             elif isinstance(value, list):
                 items = [node_class_factory(i, self) for i in value]
                 setattr(self, key, items)
-                if key in ('body', 'nodes'):
+                if key in ("body", "nodes"):
                     self.nodes = getattr(self, key)
                 self._children.update(i for i in items if isinstance(i, NodeBase))
             else:
@@ -35,9 +34,9 @@ class NodeBase:
 
     def __repr__(self):
         repr_str = f"<{self.nodeType}"
-        if hasattr(self, 'nodes'):
+        if hasattr(self, "nodes"):
             repr_str += " iterable"
-        if hasattr(self, 'type'):
+        if hasattr(self, "type"):
             if type(self.type) is str:
                 repr_str += f" {self.type}"
             else:
@@ -49,15 +48,15 @@ class NodeBase:
         return f"{repr_str}>"
 
     def _display(self):
-        if hasattr(self, 'name') and hasattr(self, 'value'):
+        if hasattr(self, "name") and hasattr(self, "value"):
             return "{} = {}".format(self.name, self.value)
-        for attr in ('name', 'value', 'absolutePath'):
+        for attr in ("name", "value", "absolutePath"):
             if hasattr(self, attr):
                 return f"{getattr(self, attr)}"
         return ""
 
     def keys(self):
-        return [i for i in dir(self) if not i.startswith('_')]
+        return [i for i in dir(self) if not i.startswith("_")]
 
     def children(
         self,
@@ -68,9 +67,9 @@ class NodeBase:
         inner_offset=None,
         outer_offset=None,
         filters={},
-        exclude={}
+        exclude={},
     ):
-        '''Get childen nodes of this node.
+        """Get childen nodes of this node.
 
         Arguments:
           depth: Number of levels of children to traverse. 0 returns only this node.
@@ -88,30 +87,25 @@ class NodeBase:
           exclude: Dictionary of {attribute:value} that children cannot match.
 
         Returns:
-            List of node objects.'''
+            List of node objects."""
         if type(filters) is dict:
             filters = [filters]
         filter_fn = functools.partial(_check_filters, inner_offset, outer_offset, filters, exclude)
-        find_fn = functools.partial(
-            _find_children,
-            filter_fn,
-            include_parents,
-            include_children
-        )
+        find_fn = functools.partial(_find_children, filter_fn, include_parents, include_children)
         result = find_fn(find_fn, depth, self)
         if include_self or not result or result[0] != self:
             return result
         return result[1:]
 
     def parents(self, depth=-1, filters={}):
-        '''Get parent nodes of this node.
+        """Get parent nodes of this node.
 
         Arguments:
             depth: Depth limit. If given as a negative value, it will be subtracted
                    from this object's depth.
             filters: Dictionary of {attribute: value} that parents must match.
 
-        Returns: list of nodes'''
+        Returns: list of nodes"""
         if type(filters) is not dict:
             raise TypeError("Filters must be a dict")
         if depth < 0:
@@ -128,7 +122,7 @@ class NodeBase:
                 return node_list
 
     def parent(self, depth=-1, filters={}):
-        '''Get a parent node of this node.
+        """Get a parent node of this node.
 
         Arguments:
             depth: Depth limit. If given as a negative value, it will be subtracted
@@ -138,7 +132,7 @@ class NodeBase:
         If a filter value is given, will return the first parent that meets the filters
         up to the given depth. If none is found, returns None.
 
-        If no filter is given, returns the parent at the given depth.'''
+        If no filter is given, returns the parent at the given depth."""
         if type(filters) is not dict:
             raise TypeError("Filters must be a dict")
         if depth < 0:
@@ -155,7 +149,7 @@ class NodeBase:
         return None
 
     def is_child_of(self, node):
-        '''Checks if this object is a child of the given node object.'''
+        """Checks if this object is a child of the given node object."""
         if node.depth >= self.depth:
             return False
         return self.parent(node.depth) == node
@@ -167,11 +161,10 @@ class NodeBase:
 
 
 class IterableNodeBase(NodeBase):
-
     def __getitem__(self, key):
         if type(key) is str:
             try:
-                return next(i for i in self.nodes if getattr(i, 'name', None) == key)
+                return next(i for i in self.nodes if getattr(i, "name", None) == key)
             except StopIteration:
                 raise KeyError(key)
         return self.nodes[key]
@@ -187,10 +180,10 @@ class IterableNodeBase(NodeBase):
 
 
 def node_class_factory(ast, parent):
-    if not isinstance(ast, dict) or 'nodeType' not in ast:
+    if not isinstance(ast, dict) or "nodeType" not in ast:
         return ast
-    name = ast['nodeType']
-    base_class = IterableNodeBase if ('nodes' in ast or 'body' in ast) else NodeBase
+    name = ast["nodeType"]
+    base_class = IterableNodeBase if ("nodes" in ast or "body" in ast) else NodeBase
     return type(name, (base_class,), {})(ast, parent)
 
 
@@ -231,11 +224,11 @@ def _find_children(filter_fn, include_parents, include_children, find_fn, depth,
 
 
 def is_inside_offset(inner, outer):
-    '''Checks if the first offset is contained in the second offset
+    """Checks if the first offset is contained in the second offset
 
     Args:
         inner: inner offset tuple
         outer: outer offset tuple
 
-    Returns: bool'''
+    Returns: bool"""
     return outer[0] <= inner[0] <= inner[1] <= outer[1]
