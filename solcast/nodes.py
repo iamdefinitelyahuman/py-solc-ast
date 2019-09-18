f168217cb005f89952753a50b64a2afbdee36d68
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-09-19 03:42:55 +0800

    various minor edits

--- solcast/nodes.py
+++ solcast/nodes.py
@@ -8,8 +8,10 @@ class NodeBase:
         self.depth = parent.depth + 1 if parent is not None else 0
         self._parent = parent
         self._children = set()
-        src = ast["src"]
+        src = [int(i) for i in ast["src"].split(":")]
         self.offset = (src[0], src[0] + src[1])
+        self.contract_id = src[2]
+
         for key, value in ast.items():
             if isinstance(value, dict) and value.get("nodeType") == "Block":
                 value = value["statements"]
@@ -37,7 +39,7 @@ class NodeBase:
         if hasattr(self, "nodes"):
             repr_str += " iterable"
         if hasattr(self, "type"):
-            if type(self.type) is str:
+            if isinstance(self.type, str):
                 repr_str += f" {self.type}"
             else:
                 repr_str += f" {self.type._display()}"
@@ -64,10 +66,10 @@ class NodeBase:
         include_self=False,
         include_parents=True,
         include_children=True,
-        inner_offset=None,
-        outer_offset=None,
-        filters={},
-        exclude={},
+        required_offset=None,
+        offset_limits=None,
+        filters=None,
+        exclude_filter=None,
     ):
         """Get childen nodes of this node.
 
@@ -78,26 +80,32 @@ class NodeBase:
                         child nodes that match.
           include_children: If True, as soon as a match is found it's children will not
                             be included in the search.
-          inner_offset: Only match nodes with a source offset that contains this offset.
-          outer_offset: Only match nodes when their source offset is contained inside
-                        this source offset.
+          required_offset: Only match nodes with a source offset that contains this offset.
+          offset_limits: Only match nodes when their source offset is contained inside
+                           this source offset.
           filters: Dictionary of {attribute: value} that children must match. Can also
                    be given as a list of dicts, children that match one of the dicts
                    will be returned.
-          exclude: Dictionary of {attribute:value} that children cannot match.
+          exclude_filter: Dictionary of {attribute:value} that children cannot match.
 
         Returns:
             List of node objects."""
-        if type(filters) is dict:
+        if filters is None:
+            filters = {}
+        if exclude_filter is None:
+            exclude_filter = {}
+        if isinstance(filters, dict):
             filters = [filters]
-        filter_fn = functools.partial(_check_filters, inner_offset, outer_offset, filters, exclude)
+        filter_fn = functools.partial(
+            _check_filters, required_offset, offset_limits, filters, exclude_filter
+        )
         find_fn = functools.partial(_find_children, filter_fn, include_parents, include_children)
         result = find_fn(find_fn, depth, self)
         if include_self or not result or result[0] != self:
             return result
         return result[1:]
 
-    def parents(self, depth=-1, filters={}):
+    def parents(self, depth=-1, filters=None):
         """Get parent nodes of this node.
 
         Arguments:
@@ -106,7 +114,7 @@ class NodeBase:
             filters: Dictionary of {attribute: value} that parents must match.
 
         Returns: list of nodes"""
-        if type(filters) is not dict:
+        if filters and not isinstance(filters, dict):
             raise TypeError("Filters must be a dict")
         if depth < 0:
             depth = self.depth + depth
@@ -121,7 +129,7 @@ class NodeBase:
             if parent.depth == depth:
                 return node_list
 
-    def parent(self, depth=-1, filters={}):
+    def parent(self, depth=-1, filters=None):
         """Get a parent node of this node.
 
         Arguments:
@@ -133,7 +141,7 @@ class NodeBase:
         up to the given depth. If none is found, returns None.
 
         If no filter is given, returns the parent at the given depth."""
-        if type(filters) is not dict:
+        if filters and not isinstance(filters, dict):
             raise TypeError("Filters must be a dict")
         if depth < 0:
             depth = self.depth + depth
@@ -162,7 +170,7 @@ class NodeBase:
 
 class IterableNodeBase(NodeBase):
     def __getitem__(self, key):
-        if type(key) is str:
+        if isinstance(key, str):
             try:
                 return next(i for i in self.nodes if getattr(i, "name", None) == key)
             except StopIteration:
@@ -187,10 +195,10 @@ def node_class_factory(ast, parent):
     return type(name, (base_class,), {})(ast, parent)
 
 
-def _check_filters(inner_offset, outer_offset, filters, exclude, node):
-    if inner_offset and not is_inside_offset(inner_offset, node.offset):
+def _check_filters(required_offset, offset_limits, filters, exclude, node):
+    if required_offset and not is_inside_offset(required_offset, node.offset):
         return False
-    if outer_offset and not is_inside_offset(node.offset, outer_offset):
+    if offset_limits and not is_inside_offset(node.offset, offset_limits):
         return False
     for f in filters:
         if _check_filter(node, f, exclude):
@@ -200,12 +208,23 @@ def _check_filters(inner_offset, outer_offset, filters, exclude, node):
 
 def _check_filter(node, filters, exclude):
     for key, value in filters.items():
-        if getattr(node, key, not value) != value:
+        if _recursive_getattr(node, key) != value:
             return False
     for key, value in exclude.items():
-        if getattr(node, key, not value) == value:
+        if _recursive_getattr(node, key) == value:
             return False
     return True
+
+
+def _recursive_getattr(obj, key):
+    if key is None:
+        raise TypeError("Cannot match against None")
+    for k in key.split("."):
+        if isinstance(obj, dict):
+            obj = obj.get(k)
+        else:
+            obj = getattr(obj, k, None)
+    return obj
 
 
 def _find_children(filter_fn, include_parents, include_children, find_fn, depth, node):
