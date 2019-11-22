5b465613b49fccb5b21b80ae254884dce1fe8e72
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-11-22 15:16:53 +0400

    add from_ast method to generate a single SourceUnit

--- solcast/__init__.py
+++ solcast/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 
-from .main import from_standard_output, from_standard_output_json  # noqa 401
+from .main import from_ast, from_standard_output, from_standard_output_json  # noqa 401
