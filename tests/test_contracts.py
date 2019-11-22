879be4fdd8d76478b78ea4327522837eb16f98bf
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2019-11-22 15:17:25 +0400

    add test case to check for mutation

--- tests/test_contracts.py
+++ tests/test_contracts.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import json
+from copy import deepcopy
 from pathlib import Path
 
 import pytest
@@ -11,3 +13,12 @@ JSON_PATHS = list(Path("tests/compiled").glob("*.json"))
 @pytest.mark.parametrize("path", JSON_PATHS)
 def test_solcast(path):
     solcast.from_standard_output_json(path)
+
+
+@pytest.mark.parametrize("path", JSON_PATHS)
+def test_mutation(path):
+    with open(path) as fp:
+        output_json = json.load(fp)
+    original = deepcopy(output_json)
+    solcast.from_standard_output(output_json)
+    assert original == output_json
